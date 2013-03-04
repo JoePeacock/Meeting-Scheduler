b80eddd911f0618d4895cdd5c61610d2e36ad875
jpeacockinc <jpeacockinc@Gmail.com>
2013-03-03 23:19:49 -0500

    Changing weeks kinda

--- static/js/base.js
+++ static/js/base.js
@@ -8,6 +8,9 @@ Page: base.html
 
 */
 
+
+/* NOTE REBUILD WEEKS WITH SINGULAR WEEKS REUPDATING THE LIST. *
+/
 var weekDay = new Array("Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat");
 var monthName = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 var date = new Date();
@@ -15,13 +18,22 @@ var pastDate = new Date();
 
 $(document).ready(function(){
 	var diff = date.getDay();
+	var ul = $('.cal-nav');
+	var calSize = 7;
+	var pos = 0;
+	var neg = 0;
+	var index = 0;
+	ul.append("<div class='week0' style='display:block'></div>");
 
-	for(i = 0; i < 21; i++) {
+	for(i = 0; i < 7; i++) {
 		var dates = new Date(date.getFullYear(), date.getMonth(), date.getDate()-diff);
-		$('.cal-nav').append('<li><span class ="date-num">' + (dates.getDate()) + '</span><span class="date-day">' + weekDay[dates.getDay()] + '<bR>' + monthName[dates.getMonth()] + '</span></li>');
-		diff--;
-		
+		$(".week0").append('<li><span class ="date-num">' + (dates.getDate()) + '</span><span class="date-day">' + weekDay[dates.getDay()] + '<bR>' + monthName[dates.getMonth()] + '</span></li>');
+		diff--;		
 	}
+
+/*	if (ul.children('li').size() > 7) {
+		ul.children('li:gt(7)', this).hide();
+*/	
 	
 	$('.cal-nav li').each(function(){
 		$(this).removeClass('cal-nav-select');
@@ -36,28 +48,55 @@ $(document).ready(function(){
 	});
 
 	$(".prev-week").click(function(){
-			var backwards = date.getDay();
-			pastDate.setDate(date.getDate()-backwards);
-		for(i = 0; i < 7; i++) {
-			pastDate.setDate(pastDate.getDate()-1);
-			$('.cal-nav').prepend('<li><span class ="date-num">' + (pastDate.getDate()) + '</span><span class="date-day">' + weekDay[pastDate.getDay()] + '<bR>' + monthName[pastDate.getMonth()] + '</span></li>');
+		index -= 1;
+		var week = parseInt($('.cal-nav div[style*="display:block"]').attr('class').charAt(4));
+		console.log(week);
+		if ((week-1) == index) {
+			$('.cal-nav').prepend("<div class='week" + index + "'></div>");
+			console.log("IM HERE");
+
+				var backwards = date.getDay();
+				pastDate.setDate(date.getDate()-backwards);
+			for(i = 0; i < 7; i++) {
+				pastDate.setDate(pastDate.getDate()-1);
+				$('.week' + index).prepend('<li><span class ="date-num">' + (pastDate.getDate()) + '</span><span class="date-day">' + weekDay[pastDate.getDay()] + '<bR>' + monthName[pastDate.getMonth()] + '</span></li>');
+			}
+			$(".cal-nav li").click(function(){
+				$(".cal-nav li").removeClass("cal-nav-select");
+				$(this).toggleClass("cal-nav-select");
+			});
+
+				$('.week' + (index+1)).hide();
+				$('.week' + (index)).fadeIn();
+
+		} else {
+			$('.week' + (index+1)).hide();
+			$('.week' + (index)).fadeIn();
 		}
-		$(".cal-nav li").click(function(){
-			$(".cal-nav li").removeClass("cal-nav-select");
-			$(this).toggleClass("cal-nav-select");
-		});
+			
 	});
-	
 
 	$(".next-week").click(function(){
-		for (i = 0; i < 7; i++) {
-			dates.setDate(dates.getDate()+1);
-			$('.cal-nav').append('<li><span class ="date-num">' + (dates.getDate()) + '</span><span class="date-day">' + weekDay[dates.getDay()] + '<bR>' + monthName[dates.getMonth()] + '</span></li>');
+		index += 1;
+
+		if ($('') == index) {
+			$('.cal-nav').append("<div class='week" + pos + "'></div>");
+
+			for (i = 0; i < 7; i++) {
+				dates.setDate(dates.getDate()+1);
+				$('.week' + pos).append('<li><span class ="date-num">' + (dates.getDate()) + '</span><span class="date-day">' + weekDay[dates.getDay()] + '<bR>' + monthName[dates.getMonth()] + '</span></li>');
+			}
+			$(".cal-nav li").click(function(){
+				$(".cal-nav li").removeClass("cal-nav-select");
+				$(this).toggleClass("cal-nav-select");
+			});
+
+			$('.week' + (index-1)).hide();
+			$('.week' + (index)).fadeIn();
+		} else {
+			$('.week' + (index-1)).hide();
+			$('.week' + (index)).fadeIn();
 		}
-		$(".cal-nav li").click(function(){
-			$(".cal-nav li").removeClass("cal-nav-select");
-			$(this).toggleClass("cal-nav-select");
-		});
 
 	});
 });
